d5f9a7bbb04b6dcde1ce9fdb84b497b603ac56d2
umaryousaf <65030709+umaryousaf@users.noreply.github.com>
2020-06-10 19:34:29 +0500

    Update chapter2.js

--- chapter2.js
+++ chapter2.js
@@ -1,32 +1,36 @@
 // PDF FILE # 2
 
+// Task 1
 var userName = "Umar";
 var y = "Yousaf";
 var z = userName + " " + y;
 alert(z);
 
-
+// Task 2
 var Age = 17;
 var z = "My age is" + " " + Age;
 alert(z);
 
-
+// Task 3
 alert("Certified Mobile Application Development");
 
-
+// Task 4
 var name = ["PIZZA\nPIZZ\nPIZ\nPI\nP"];
 alert(name);
 
+// Task 5
 var email = "example@example.com";
 alert(email);
 
+// Task 6
 var book = "I am trying to learn from the book A smarter way to learn JavaScript."
 alert(book);
 
-
+// Task 7
 var content = "Yah! I can write HTML content through JavaScript";
 document.write(content);
 
+// Task 8
 var line = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
 alert(line);
 document.write(line);
