a71702edd0421d13b1b576584d0f3671692a83c4
umaryousaf <65030709+umaryousaf@users.noreply.github.com>
2020-06-10 19:32:13 +0500

    Update chapter1.js

--- chapter1.js
+++ chapter1.js
@@ -1,11 +1,17 @@
 // PDF FILE # 1
 
+// Task 1
 alert("Error! Please Enter a valid Password");
 
+// Task 2
 alert("Welcome to Js Land...\nHappy Coding!");
 
+// Task 3
 alert("Welcome to Js land");
 
+// Task 4
 alert("Happy Coding!\n + Prevent this page from creating additional dialogs");
 
+// Task 5
 alert("Hello... I can run Js through my web browser's console. ");
+
